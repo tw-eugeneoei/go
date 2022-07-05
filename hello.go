3a6cc8bdf28c9bcc4090e62e9019eedb4856c065
Eugene Oei <eugene.oei@thoughtworks.com>
2022-07-05 15:56:13 +0800

    improve test case and refactor basic programme

--- hello.go
+++ hello.go
@@ -2,10 +2,15 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		return englishHelloPrefix + "World"
+	}
+	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("asd"))
+	fmt.Println(Hello("World"))
 }
--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,25 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	// * declare functions inside other functions and assign them to variables
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		// * t.Helper() is needed to tell the test suite that this method is a helper. By doing this when it fails the line number reported will be in our function call rather than inside our test helper
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, world' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
 }
